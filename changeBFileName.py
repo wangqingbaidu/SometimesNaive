3be0e8c39c66d8f8620424b9071986df4eaa619a
张志伟 <564326047@qq.com>
2017-07-02 10:06:01 +0800

    Update changeBFileName.py

--- changeBFileName.py
+++ changeBFileName.py
@@ -9,7 +9,7 @@ From Institute of Computing Technology
 '''
 import os, argparse, re
 parser = argparse.ArgumentParser("Change XXX@XXX@(XXX) file name.")
-parser.add_argument('-dir', default='h:\\new', help='Directory of file.')
+parser.add_argument('-dir', default='l:\\clean', help='Directory of file.')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -17,16 +17,31 @@ if __name__ == '__main__':
     if os.path.exists(args.dir) and os.path.isdir(args.dir):
         for f in os.listdir(args.dir):
             if (os.path.isdir(os.path.join(args.dir,f))):
-                new_dir_name = '-'.join(re.split("\(|\)", f)[-2:])
-                for sub_file in os.listdir(os.path.join(args.dir, f)):
-                    try:
-                        os.rename(os.path.join(args.dir, f, sub_file), 
-                                  os.path.join(args.dir, f, '-'.join(re.split("\(|\)", sub_file))))
-                    except:
-                        print 'Error dealing with file',  sub_file
-                
-                os.rename(os.path.join(args.dir, f), 
-                          os.path.join(args.dir, new_dir_name))
-                          
-    
+                try:
+                    items = re.split("\(|\)", f)
+                    bw_comp=items[1]
+                    bw_num = items[3]
+                    bw_name = items[4]
+                    new_name = bw_num + '-' + bw_name
+                    if len(os.listdir(os.path.join(args.dir, f))) == 2:
+                        for sub_file in os.listdir(os.path.join(args.dir, f)):
+                            file_extension = os.path.splitext(sub_file)[1] 
+                            os.rename(os.path.join(args.dir, f, sub_file), 
+                                      os.path.join(args.dir, f, new_name + file_extension))
+                        
+                        os.rename(os.path.join(args.dir, f), 
+                                  os.path.join(args.dir, new_name))
+                    elif len(os.listdir(os.path.join(args.dir, f))) == 3:
+                        print f
+                        for sub_file in os.listdir(os.path.join(args.dir, f)):
+                                os.rename(os.path.join(args.dir, f, sub_file), 
+                                          os.path.join(args.dir, f, '-'.join(re.split("\(|\)", sub_file)[-2:])))
+                        os.rename(os.path.join(args.dir, f), 
+                                  os.path.join(args.dir, new_name))
+                    else:
+                        print f
+                  
+                except Exception,e:
+                    print 'Error dealing with file',  f, e
+
     
