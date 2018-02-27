23657ca751a81f8e61a2f57f394eddc50baaae6f
张志伟 <564326047@qq.com>
2018-02-27 18:09:05 +0800

    Create rm_duplicate.py

---
+++ rm_duplicate.py
@@ -0,0 +1,17 @@
+import sys
+data = {}
+with open(sys.argv[1]) as f:
+	for l in f.readlines():
+		photoid, flag = l.strip().split()
+		flag = True if flag == 'true' else False
+		data[photoid] = flag
+
+moving_data = open(sys.argv[3], 'w')
+
+with open(sys.argv[2]) as f:
+	for l in f.readlines():
+		photoid, c = l.strip().split()
+		if photoid in data and data[photoid]:
+			pass
+		else:
+			moving_data.write('%s %s\n' %(photoid, c))
