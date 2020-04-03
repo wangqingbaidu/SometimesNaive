ce7670f08915b4ebb5c0fcb5bb484cd98d8b9bc7
张志伟 <wangqingbaidu@gmail.com>
2020-04-03 22:57:55 +0800

    Update rmNone.py

--- rmNone.py
+++ rmNone.py
@@ -8,3 +8,15 @@ for i in images:
 	if pid not in photoid:
 		print ('rm %s' %i)
 		#os.system('rm %s' %i)
+{'features': [{
+    'feat_file': os.path.join(train_data_dir, 'topic_features.txt'),
+    'attach_column': [2, 1],
+    'feat_length': 14,
+    'feat_format': 'topic',
+    'separator': '\t',
+    'feat_type': float,
+    'missing_value': -999
+    # 'missing_value': [-999] + [0] * 13
+    # 'missing_value': [-999] + [None, None] + [0] * 11,
+    'attach_only_has_key': False
+}]}
